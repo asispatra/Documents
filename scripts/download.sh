2388759c37afba57056d4920f2b0bc7557fd0751
Asis Patra <asis.patra0425@gmail.com>
2020-04-09 18:04:38 +0530

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+CURRENT_DIR=$PWD
+mkdir -p ~/bin/
+cd ~/bin/
+
 wget "https://github.com/asispatra/Documents/blob/master/scripts/create"
+wget "https://raw.githubusercontent.com/asispatra/Documents/master/scripts/cdm2c"
+
+cd ${CURRENT_DIR}
