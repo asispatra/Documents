2cf9bb9bf53551f714fb64498fb35d819863afa6
Asis Patra <asis.patra0425@gmail.com>
2020-05-12 11:20:15 +0530

    Create download_run_scripts.sh

---
+++ scripts/download_run_scripts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget "https://raw.githubusercontent.com/asispatra/Documents/master/scripts/run.sh" -O run.sh
+wget "https://raw.githubusercontent.com/asispatra/Documents/master/scripts/helper.sh" -O helper.sh
