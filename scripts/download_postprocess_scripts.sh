736c5424dfcdaacc63aaad3114c5a6a27c32dca7
Asis Patra <asis.patra0425@gmail.com>
2020-05-13 13:25:21 +0530

    Create download_postprocess_scripts.sh

---
+++ scripts/download_postprocess_scripts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget "https://raw.githubusercontent.com/asispatra/Documents/master/scripts/csv2xlsx.py" -O csv2xlsx.py
+wget "https://raw.githubusercontent.com/asispatra/Documents/master/scripts/getdata_fromFiles.sh" -O getdata_fromFiles.sh
